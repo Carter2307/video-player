53dd42b785a9b9b2d9b417b7d44f21271c064615
Carter2307 <remirjunior1999@gmail.com>
2022-05-05 21:49:27 -0400

    audio player stuff

--- App/classes/Mediaplayer.js
+++ App/classes/Mediaplayer.js
@@ -8,97 +8,100 @@ import Animation from "../components/Animation"
 
 export default class MediaPlayer extends HTMLElement {
   constructor() {
-    super()
-    //this.shadow = this.attachShadow({ mode: "open" })
+	 super()
+	 //this.shadow = this.attachShadow({ mode: "open" })
   }
 
   connectedCallback() {
-     this.show = 5000
-     this.timeOudId = 0
-    this.type = this.getAttribute("type")
-    this.src = this.getAttribute("src")
+	  this.show = 5000
+	  this.timeOudId = 0
+	 this.type = this.getAttribute("type")
+	 this.src = this.getAttribute("src")
 
-    this.setAttribute("class", "videoplayer")
+	 this.type === "video" ? this.setAttribute("class", "videoplayer") : this.setAttribute("class", "audioplayer")
 
-    this.#create(this.src, this.type)
-    this.scale = new Animation()
-    this.#addEventListener()
+	 this.#create(this.src, this.type)
+	 this.scale = new Animation()
+	 this.#addEventListener()
   }
 
   #getElemnts() {
-    this.elements = {
-       controlers : this.querySelector(".videoplayer__controls"),
-       mobile : {
-          menu : document.querySelector(".mobile__menu__buttons"),
-          button : document.querySelector(".mobile__menu__button")
-       },
-      navigation: {
-        play: document.querySelector("[data-icon=\"icon-Play\"]"),
-        rwd: Array.from(document.querySelectorAll("[data-icon=\"icon-backward-15-seconds\"]")),
-        fwd: Array.from(document.querySelectorAll("[data-icon=\"icon-forward-15-seconds\"]")),
-      },
-      volume: {
-        button: document.querySelector("[data-icon=\"icon-volume-high\"]"),
-        range: document.querySelector(".controlers__volume__handler__range"),
-        track: document.querySelector(".controlers__volume__handler__elapse"),
-      },
-      timer: {
-        starttime: document.querySelector(".timer__starttime"),
-        endtime: document.querySelector(".timer__endtime"),
-        range: document.querySelector(".timer__progress__range"),
-        track: document.querySelector(".timer__progress__elapse"),
-      },
-      other: {
-        pnp: document.querySelector("[data-icon='icon-screenmirroring']"),
-        fullscreen: Array.from(document.querySelectorAll("[data-icon='icon-maximize-circle']")),
-      },
-    }
+	 this.elements = {
+		 controlers : this.querySelector(".videoplayer__controls"),
+		 mobile : {
+			 menu : document.querySelector(".mobile__menu__buttons"),
+			 button : document.querySelector(".mobile__menu__button")
+		 },
+		navigation: {
+		  play: document.querySelector("[data-icon=\"icon-Play\"]"),
+		  rwd: Array.from(document.querySelectorAll("[data-icon=\"icon-backward-15-seconds\"]")),
+		  fwd: Array.from(document.querySelectorAll("[data-icon=\"icon-forward-15-seconds\"]")),
+		},
+		volume: {
+		  button: document.querySelector("[data-icon=\"icon-volume-high\"]"),
+		  range: document.querySelector(".controlers__volume__handler__range"),
+		  track: document.querySelector(".controlers__volume__handler__elapse"),
+		},
+		timer: {
+		  starttime: document.querySelector(".timer__starttime"),
+		  endtime: document.querySelector(".timer__endtime"),
+		  range: document.querySelector(".timer__progress__range"),
+		  track: document.querySelector(".timer__progress__elapse"),
+		},
+		other: {
+		  pnp: document.querySelector("[data-icon='icon-screenmirroring']"),
+		  fullscreen: Array.from(document.querySelectorAll("[data-icon='icon-maximize-circle']")),
+		},
+	 }
   }
 
   #create(src, type) {
-    this.media = this.#createMedia(src, type)
-    this.media.setAttribute("playsinline", "")
+	 this.media = this.#createMedia(src, type)
+	 this.media.setAttribute("playsinline", "")
 
-    this.#createControls()
-    this.#getElemnts()
-    this.#createNavigation()
-    this.#createVolume()
-    this.#createTimer()
+	 this.#createControls()
+	 this.#getElemnts()
+	 this.#createNavigation()
+	 this.#createVolume()
+	 this.#createTimer()
   }
 
   #createMedia(src, type) {
-    let element
-    let ext = getExtension(src)
-    let source = document.createElement("source")
+	 let element
+	 let ext = getExtension(src)
+	 let source = document.createElement("source")
 
-    if (type === "audio") {
-      element = document.createElement("audio")
-      source.setAttribute("type", `audio/${ext}`)
-    } else if (type === "video") {
-      element = document.createElement("video")
-      source.setAttribute("type", `video/${ext}`)
-    } else {
-      console.error(`the type : ${type} provided is not support`)
-      return null
-    }
+	 if (type === "audio") {
+		element = document.createElement("audio")
+		element.setAttribute("class", "audioplayer__audio")
+		source.setAttribute("type", `audio/${ext}`)
+	 } else if (type === "video") {
+		element = document.createElement("video")
+		element.setAttribute("class", "videoplayer__video")
+		source.setAttribute("type", `video/${ext}`)
+	 } else {
+		console.error(`the type : ${type} provided is not support`)
+		return null
+	 }
 
-    if (element) {
-      element.setAttribute("class", "videoplayer__video")
-      source.src = src
-      element.append(source)
-    }
+	 if (element) {
+		//element.setAttribute("class", "videoplayer__video")
+		source.src = src
+		element.append(source)
+	 }
 
-    this.append(element)
+	 this.append(element)
 
-    return element
-    //this.shadow.append(element)
+	 return element
+	 //this.shadow.append(element)
   }
 
   #createControls() {
-    let element = document.createElement("div")
-    element.setAttribute("class", "videoplayer__controls")
-    element.innerHTML = `
-    <!--Controlers -->
+	  let element = document.createElement("div")
+	  // eslint-disable-next-line no-mixed-spaces-and-tabs
+	 this.type === "audio" ? element.setAttribute("class", "audioplayer__controls") : element.setAttribute("class", "videoplayer__controls")
+	 element.innerHTML = `
+	 <!--Controlers -->
 						<div class="controlers">
 							<!--Volume -->
 							<div class="controlers__volume">
@@ -156,7 +159,7 @@ export default class MediaPlayer extends HTMLElement {
 									class="timer__progress__range"
 									min="0"
 									max="100"
-                           value="0"
+									value="0"
 									step="1" />
 								<span class="timer__progress__bar"></span>
 								<span class="timer__progress__elapse"></span>
@@ -170,108 +173,108 @@ export default class MediaPlayer extends HTMLElement {
 							</button>
 							<ul class="mobile__menu__buttons">
 								<il class="mobile__menu__buttons__item" data-icon="icon-forward-15-seconds">
-                           <span class="mobile__menu__buttons__item__text" ">Forward</span>
-                           <i class="mobile__menu__buttons__item__icon icon-forward-15-seconds"></i>
-                        </il>
-                        <il class="mobile__menu__buttons__item" data-icon="icon-backward-15-seconds">
-                           <span class="mobile__menu__buttons__item__text">Backward</span>
-                           <i class="mobile__menu__buttons__item__icon icon-backward-15-seconds"></i>
-                        </il>
-                        <il class="mobile__menu__buttons__item" data-icon="icon-maximize-circle">
-                           <span class="mobile__menu__buttons__item__text">Fullscreen</span>
-                           <i class="mobile__menu__buttons__item__icon icon-maximize-circle"></i>
-                        </il>
+									<span class="mobile__menu__buttons__item__text" ">Forward</span>
+									<i class="mobile__menu__buttons__item__icon icon-forward-15-seconds"></i>
+								</il>
+								<il class="mobile__menu__buttons__item" data-icon="icon-backward-15-seconds">
+									<span class="mobile__menu__buttons__item__text">Backward</span>
+									<i class="mobile__menu__buttons__item__icon icon-backward-15-seconds"></i>
+								</il>
+								<il class="mobile__menu__buttons__item" data-icon="icon-maximize-circle">
+									<span class="mobile__menu__buttons__item__text">Fullscreen</span>
+									<i class="mobile__menu__buttons__item__icon icon-maximize-circle"></i>
+								</il>
 							</ul>
 						</div>
 `
-    this.append(element)
-    //this.shadow.append(element)
+	 this.append(element)
+	 //this.shadow.append(element)
   }
 
   #createNavigation() {
-    this.navigation = new Navigation(
-      this.elements.navigation.play,
-      this.elements.navigation.rwd,
-      this.elements.navigation.fwd,
-      this.media
-    )
+	 this.navigation = new Navigation(
+		this.elements.navigation.play,
+		this.elements.navigation.rwd,
+		this.elements.navigation.fwd,
+		this.media
+	 )
   }
 
   #createVolume() {
-    this.volume = new Volume(
-      this.elements.volume.button,
-      this.elements.volume.range,
-      this.elements.volume.track,
-      this.media
-    )
+	 this.volume = new Volume(
+		this.elements.volume.button,
+		this.elements.volume.range,
+		this.elements.volume.track,
+		this.media
+	 )
   }
 
   #createTimer() {
-    this.timer = new Timer(
-      this.elements.timer.starttime,
-      this.elements.timer.endtime,
-      this.elements.timer.range,
-      this.elements.timer.track,
-      this.media
-    )
+	 this.timer = new Timer(
+		this.elements.timer.starttime,
+		this.elements.timer.endtime,
+		this.elements.timer.range,
+		this.elements.timer.track,
+		this.media
+	 )
   }
 
   #onFullScreen(e) {
-     const btn = e.currentTarget
-     const icon = btn.querySelector("i")
-     this.#fullscreenHandler(btn, icon)
+	  const btn = e.currentTarget
+	  const icon = btn.querySelector("i")
+	  this.#fullscreenHandler(btn, icon)
   }
 
   #fullscreenHandler(btn, icon) {
-     if (!document.fullscreenElement) {
-        btn.setAttribute("data-icon", "icon-minimize-circle")
-        icon.setAttribute("class", "icon-minimize-circle")
-        this.requestFullscreen({ navigationUI: "hide" })
-     } else {
-        btn.setAttribute("data-icon", "icon-maximize-circle")
-        icon.setAttribute("class", "icon-maximize-circle")
-        document.exitFullscreen()
-     }
+	  if (!document.fullscreenElement) {
+		  btn.setAttribute("data-icon", "icon-minimize-circle")
+		  icon.setAttribute("class", "icon-minimize-circle")
+		  this.requestFullscreen({ navigationUI: "hide" })
+	  } else {
+		  btn.setAttribute("data-icon", "icon-maximize-circle")
+		  icon.setAttribute("class", "icon-maximize-circle")
+		  document.exitFullscreen()
+	  }
   }
 
   #onPictureInPicture() {
-    this.media.requestPictureInPicture().then((picture) => {
-      console.log("picture in picture" + picture)
-    })
+	 this.media.requestPictureInPicture().then((picture) => {
+		console.log("picture in picture" + picture)
+	 })
   }
 
   #mobileHandler() {
-     this.elements.mobile.menu.classList.toggle("-isVisible")
+	  this.elements.mobile.menu.classList.toggle("-isVisible")
 }
 
 #onMouseEnter () {
-   if(this.timeOudId !== 0) clearTimeout(this.timeOudId)
-   this.elements.controlers.classList.remove("-hidden")
+	if(this.timeOudId !== 0) clearTimeout(this.timeOudId)
+	this.elements.controlers.classList.remove("-hidden")
 }
 
 #onMouseLeave () {
-   setTimeout(() => {
-      this.elements.controlers.classList.add("-hidden")
-   },this.show)
+	setTimeout(() => {
+		this.elements.controlers.classList.add("-hidden")
+	},this.show)
   }
 
-   #onMouseOver () {
-     if(this.event) console.log("true")
-      setTimeout(() => {
-         this.elements.controlers.classList.add("-hidden")
-      },this.show)
-   }
+	#onMouseOver () {
+	  if(this.event) console.log("true")
+		setTimeout(() => {
+			this.elements.controlers.classList.add("-hidden")
+		},this.show)
+	}
 
   #addEventListener() {
-     this.addEventListener("mouseenter", this.#onMouseEnter.bind(this))
-     this.addEventListener("mouseleave", this.#onMouseLeave.bind(this))
-     //this.addEventListener("mouseover", this.#onMouseOver.bind(this))
+	  this.addEventListener("mouseenter", this.#onMouseEnter.bind(this))
+	  this.addEventListener("mouseleave", this.#onMouseLeave.bind(this))
+	  //this.addEventListener("mouseover", this.#onMouseOver.bind(this))
 
-    this.elements.other.fullscreen.forEach(button => {
-       button.addEventListener("click", this.#onFullScreen.bind(this))
-    })
-    this.elements.other.pnp.addEventListener("click", this.#onPictureInPicture.bind(this))
-     this.elements.mobile.button.addEventListener("click", this.#mobileHandler.bind(this))
+	 this.elements.other.fullscreen.forEach(button => {
+		 button.addEventListener("click", this.#onFullScreen.bind(this))
+	 })
+	 this.elements.other.pnp.addEventListener("click", this.#onPictureInPicture.bind(this))
+	  this.elements.mobile.button.addEventListener("click", this.#mobileHandler.bind(this))
   }
 
 
